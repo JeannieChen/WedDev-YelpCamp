448590552d7198b6e967521982f7d51c0c15e98c
root <jeannie_chen@hotmail.com>
2020-06-01 19:26:20 +0000

    finish RESTful routing

--- app.js
+++ app.js
@@ -14,23 +14,25 @@ app.set("view engine", "ejs");
 // Schema setup
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
-// 	{
-// 		name: "Clear Creek", 
-// 		 image: "https://images.unsplash.com/photo-1578582805561-4abd22211b9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+// {
+// 	name: "Clear Creek", 
+//  	image: "https://images.unsplash.com/photo-1578582805561-4abd22211b9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+// 	description: "Love this place!!",
 // 	}, function(err, campground){
-// 		if(err){
-// 			console.log(err);
-// 		}else{
-// 			console.log("NEW CAMPGROUND: ");
-// 			console.log(campground);
-// 		}
-// }
-// )
+// 	if(err){
+//  			console.log(err);
+//  		}else{
+//  			console.log("NEW CAMPGROUND: ");
+//  			console.log(campground);
+//  		}
+//  }
+//  )
 
 
 // Landing page: root
@@ -44,7 +46,7 @@ app.get("/campgrounds", function(req, res){
 		if(err){
 			console.log(err);
 		} else{
-			res.render("campgrounds", {campgrounds:  allcamps});
+			res.render("index", {campgrounds:  allcamps});
 		}
 	});
 });
@@ -53,7 +55,8 @@ app.get("/campgrounds", function(req, res){
 app.post("/campgrounds", function(req,res){
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc};
 	
 	// Create a new campground and save to DB 	
 	Campground.create(newCampground, function(err, newCamp){
@@ -70,6 +73,17 @@ app.post("/campgrounds", function(req,res){
 
 app.get("/campgrounds/new", function(req, res){
 	res.render("new.ejs");
+});
+
+// Show description
+app.get("/campgrounds/:id", function(req, res){
+	Campground.findById(req.params.id, function(err, foundCamp){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("show", {campground: foundCamp});
+		}
+	});
 });
 
 // listener
