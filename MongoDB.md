0fa348e3c22b70ff1bb161c5c5f65d5e9b0a4070
root <jeannie_chen@hotmail.com>
2020-06-01 18:50:53 +0000

    setup mongoDB and mongoose

---
+++ MongoDB.md
@@ -0,0 +1,14 @@
+## Common Mongo Commands
+
+* ./mongod: to get started
+* mongo: starts shell
+
+* help: view commands
+* use (db name): switch to DB 
+
+* insert - db.(name).insert: add new
+* find - db.(name).find(): return all in (name)
+* update - db.(name).update({key: ""}, {$set: {...}}) // update item with that key, update metrics in {$set: }
+* remove - db.(name).remove({key: ""})
+
+
--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-var express  = require("express");
-var app = express();
-var bodyparser = require("body-parser");
+var express  = require("express"),
+	app = express(),
+	bodyparser = require("body-parser"),
+	mongoose = require("mongoose");
 
-var campgrounds = [
-	{name: "Salmon Creek", image: "https://images.unsplash.com/photo-1547041270-d3d54e1263cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-	{name: "Clear Creek", image: "https://images.unsplash.com/photo-1578582805561-4abd22211b9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-	{name: "Alum Creek", image: "https://images.unsplash.com/photo-1582570870711-a3f4a57d1dce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-	{name: "Clear Creek", image: "https://images.unsplash.com/photo-1578582805561-4abd22211b9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-	{name: "Alum Creek", image: "https://images.unsplash.com/photo-1582570870711-a3f4a57d1dce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"}
-]
+// Connect to DB 
+mongoose.connect("mongodb://localhost/yelp_camp");
 
+// JSON Parser & view engine setup
 app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+
+// Schema setup
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: "Clear Creek", 
+// 		 image: "https://images.unsplash.com/photo-1578582805561-4abd22211b9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+// 	}, function(err, campground){
+// 		if(err){
+// 			console.log(err);
+// 		}else{
+// 			console.log("NEW CAMPGROUND: ");
+// 			console.log(campground);
+// 		}
+// }
+// )
 
 
 // Landing page: root
@@ -22,7 +40,13 @@ app.get("/", function(req, res){
 
 // Camp route
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds:  campgrounds});
+	Campground.find({}, function(err, allcamps){
+		if(err){
+			console.log(err);
+		} else{
+			res.render("campgrounds", {campgrounds:  allcamps});
+		}
+	});
 });
 
 // Post: user submit campgrounds
@@ -30,9 +54,17 @@ app.post("/campgrounds", function(req,res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	// re-redict
-	res.redirect("/campgrounds");
+	
+	// Create a new campground and save to DB 	
+	Campground.create(newCampground, function(err, newCamp){
+			if(err){
+				console.log(err);
+			}else{
+				// Redirect to campgrounds 			
+				res.redirect("/campgrounds");
+			}
+		}
+	)
 });
 
 
