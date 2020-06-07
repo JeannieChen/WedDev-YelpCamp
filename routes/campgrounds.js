f3e25f2db91b201c00776bbc8020bf447a19e9d5
root <jeannie_chen@hotmail.com>
2020-06-07 23:28:50 +0000

    asscociate campground and users

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -28,7 +28,12 @@ router.post("/campgrounds", isLoggedIn, function(req,res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
-	var newCampground = {name: name, image: image, description: desc};
+
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+	var newCampground = {name: name, image: image, description: desc, author: author};
 	
 	// Create a new campground and save to DB 	
 	Campground.create(newCampground, function(err, newCamp){
